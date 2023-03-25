88dd1635f8f261ed39a233251fb975c20f97c089
chenpnn <chpeng2535@gmail.com>
2023-03-25 14:28:35 +0800

    add TradeEnv class

--- env/TradeEnv.py
+++ env/TradeEnv.py
@@ -1,3 +1,36 @@
 import numpy as np
 import pandas as pd
 import gymnasium as gym
+from gymnasium import spaces
+
+
+class TradeEnv(gym.Env):
+    """A customed stock trading environment based on OpenAI gymnasium"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, 
+                 df_macro,
+                 df_factor,
+                 df_stock,
+        ):
+        super(TradeEnv, self).__init__()
+
+        self.df_macro = df_macro
+        self.df_factor = df_factor
+        self.df_stock = df_stock
+
+
+    def _next_observation(self):
+        pass
+
+    def _take_action(self, action):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human', close=False):
+        pass
